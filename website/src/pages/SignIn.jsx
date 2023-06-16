381c6016c83c46a2aa6485820df82dfad5da6510
Tersoo <tersooahire21@gmail.com>
2023-06-16 14:32:27 +0100

    created left section of sign in page

--- website/src/pages/SignIn.jsx
+++ website/src/pages/SignIn.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom"
+import logo from "/logo.svg";
+import apple from "../assets/signin/apple.svg";
+import google from "../assets/signin/google.svg";
+import tick from "../assets/signin/tick.svg";
 
-export default function Signin(){
+
+export default function Signin() {
     return (
-        <>
-            <h1>
-                Sign In
-            </h1>
-        </>
-    )
+        // SIGN IN PAGE
+        <section className="sign-in">
+            <div className="left">
+                <img src={logo} alt="Coonected Logo" />
+                <div className="sub">
+                    <h1>Sign In</h1>
+                    <p>Welcome back, you’ve been missed!</p>
+                    <div className="social-container">
+                        <button className="social-btn1">
+                            <img src={apple} alt="Apple Icon" />
+                            Login via Apple
+                        </button>
+                        <button className="social-btn2">
+                            <img src={google} alt="Google Icon" />
+                            Login via Google
+                        </button>
+                    </div>
+                    <p>OR</p>
+                    <input type="email" name="email" placeholder="johndoes@gmail.com" required />
+                    <input type="password" name="pasword" placeholder="password" required />
+                    <div className="checkbox-container">
+                        <div id="remember">
+                            <img src={tick} alt="Tick Icon" />
+                            <p>Remember me</p>
+                        </div>
+                        <Link to="#" >Forgot Password?</Link>
+                    </div>
+                    <button className="submit">Sign In</button>
+                </div>
+            </div>
+
+            <div className="right"></div>
+        </section>
+    );
 }
