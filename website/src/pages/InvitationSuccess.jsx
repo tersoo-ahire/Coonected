d8e124e45f6a4e072f594df1e526bcbc3d40148c
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-06-18 18:11:29 +0100

    finished invitation success page

--- website/src/pages/InvitationSuccess.jsx
+++ website/src/pages/InvitationSuccess.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import success from "../assets/invitation/success.svg";
 
 export default function Success(){
     return (
-        <div>
-            Hi
+        // INVITATION SUCCESS PAGE
+        <div className="invitation-page">
+            <div className="sub2">
+                <h1>Success</h1>
+                <p>Invitation successfully sent!</p>
+                <img src={success} alt="Success Image" />
+                <Link to="/home">
+                    Return to Homepage
+                </Link>
+            </div>
         </div>
     )
 }
