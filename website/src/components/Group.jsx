621f429704fed506a9ac7d5a9f113210fdbbfa37
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-06-20 16:12:35 +0100

    created group component

--- website/src/components/Group.jsx
+++ website/src/components/Group.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Group(props){
     return (    
         <div className="group-list">
             <img src={props.icon} alt={props.group_name} />
-            <p>{props.group_name}</p>
+            <Link to="#">{props.group_name}</Link>
         </div>
     )
 }
