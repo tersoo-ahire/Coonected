f3b145a9a96fa9e7d0d17260ba3f7b092e59931e
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-06-20 20:26:09 +0100

    implemented connection section on the home page

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Search from "../components/Search";
 import Group from "../components/Group";
 import groupdata from "../data/group";
+import Connections from "../components/Coonections";
+import connectiondata from "../data/connections";
 
 export default function Home(){
     const grouplist = groupdata.map(item => (<Group key={item.id} {...item} />))
+    const connectionlist = connectiondata.map(item => (<Connections key={item.id} {...item}/>))
 
     return (
         // HOME PAGE SECTION
@@ -14,6 +17,12 @@ export default function Home(){
                 <div className="group-container">
                     <h2>YOUR GROUP</h2>
                     {grouplist}
+                </div>
+                <div className="connection-container">
+                    <h2>COONECTIONS</h2>
+                    <div className="connection-sub-container">
+                        {connectionlist}
+                    </div>
                 </div>
             </div>
             <div className="center">
