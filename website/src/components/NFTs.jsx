088ccded95f49f4c7db0efd46ce54e5bac6b898a
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-01 03:56:40 +0100

    created nft cards

--- website/src/components/NFTs.jsx
+++ website/src/components/NFTs.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import NFT from "./NFT";
+import nftdata from "../data/nfts";
 
 export default function NFTs(){
+    const nftslist = nftdata.map(item => (<NFT key={item.id} {...item} />))
+
     return (
         <div className="nfts-container">
-            NFT
+            {nftslist}
         </div>
     )
 }
--- website/src/data/nfts.js
+++ website/src/data/nfts.js
@@ -9,59 +9,67 @@ import nft8 from "../assets/nfts/nft8.png";
 import nft9 from "../assets/nfts/nft9.png";
 
 export default [
-    {
-      id: 1,
-      nft_collection: "CryptoGuard",
-      nft_name: "Ape Series",
-      nft_price: 0.057,
-    },
-    {
-      id: 2,
-      nft_collection: "Ape Series",
-      nft_name: "CryptoGuard #4980",
-      nft_price: 0.057,
-    },
-    {
-      id: 3,
-      nft_collection: "Ape Series",
-      nft_name: "Bored Ape #4980",
-      nft_price: 0.057,
-    },
-    {
-      id: 4,
-      nft_collection: "Bored Ape",
-      nft_name: "Bored Ape #4983",
-      nft_price: 0.057,
-    },
-    {
-      id: 5,
-      nft_collection: "CryptoGuard",
-      nft_name: "Ape Series",
-      nft_price: 0.057,
-    },
-    {
-      id: 6,
-      nft_collection: "The Bears",
-      nft_name: "Lion King",
-      nft_price: 0.057,
-    },
-    {
-      id: 7,
-      nft_collection: "Bored Ape",
-      nft_name: "Bored Ape #4870",
-      nft_price: 0.057,
-    },
-    {
-      id: 8,
-      nft_collection: "Majestic Bears",
-      nft_name: "Bored Ape #4980",
-      nft_price: 0.057,
-    },
-    {
-      id: 9,
-      nft_collection: "Lucky Lion",
-      nft_name: "Bored Ape #4983",
-      nft_price: 0.057,
-    },
+  {
+    id: 1,
+    nft_image: nft1,
+    nft_collection: "CryptoGuard",
+    nft_name: "Ape Series",
+    nft_price: 0.057,
+  },
+  {
+    id: 2,
+    nft_image: nft2,
+    nft_collection: "Ape Series",
+    nft_name: "CryptoGuard #4980",
+    nft_price: 0.057,
+  },
+  {
+    id: 3,
+    nft_image: nft3,
+    nft_collection: "Ape Series",
+    nft_name: "Bored Ape #4980",
+    nft_price: 0.057,
+  },
+  {
+    id: 4,
+    nft_image: nft4,
+    nft_collection: "CryptoGuard",
+    nft_name: "CryptoGuard #4980",
+    nft_price: 0.057,
+  },
+  {
+    id: 5,
+    nft_image: nft5,
+    nft_collection: "CryptoGuard",
+    nft_name: "Bored Ape #4980",
+    nft_price: 0.057,
+  },
+  {
+    id: 6,
+    nft_image: nft6,
+    nft_collection: "CryptoGuard",
+    nft_name: "Bored Ape #4983",
+    nft_price: 0.057,
+  },
+  {
+    id: 7,
+    nft_image: nft7,
+    nft_collection: "Ape Series",
+    nft_name: "0.057",
+    nft_price: 112,
+  },
+  {
+    id: 8,
+    nft_image: nft8,
+    nft_collection: "Ape Series",
+    nft_name: "0.057",
+    nft_price: 112,
+  },
+  {
+    id: 9,
+    nft_image: nft9,
+    nft_collection: "Ape Series",
+    nft_name: "0.057",
+    nft_price: 112,
+  },
 ];
-  
