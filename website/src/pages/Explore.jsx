1047826cba9953485061b605ae346145ec6e7e44
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-04 13:32:00 +0100

    implemented right secion and top area ofthe explore page

--- website/src/pages/Explore.jsx
+++ website/src/pages/Explore.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Search from "../components/Search";
+import Userprofile from "../components/Userprofile";
+import Activities from "../components/Activities";
+import Navigation from "../components/Navigation";
 
 export default function Explore() {
     // EXPLORE PAGE SECTION
     return (
         <section className="explore-page">
-            Explore
+            <div className="left-section">
+                <div className="top-area">
+                    <Search/>
+                    <Navigation/>
+                </div>
+                <div className="bottom-area">
+
+                </div>
+            </div>
+            <div className="right-section">
+                <Userprofile/>
+                <Activities/>
+            </div>
         </section>
     )
 }
