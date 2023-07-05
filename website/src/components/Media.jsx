cd2423b4df12645ba5f509e2711e353427d6b5ab
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-05 12:36:39 +0100

    finished media component rendered in the explore page

--- website/src/components/Media.jsx
+++ website/src/components/Media.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import MediaImage from "./MediaImage"
+import mediadata from "../data/mediadata"
 
 export default function Media(){
+    const medialist = mediadata.map(item => (<MediaImage key={item.id} {...item} />))
+
     return (
-        <div>
-            Media
+        <div className="media-container">
+            {medialist}
         </div>
     )
 }
---
+++ website/src/components/MediaImage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import add from "../assets/media/add.svg";
+
+export default function MediaImage(props){
+    return (
+        <div className="media-sub">
+            <img src={props.image} alt={props.username} />
+            <div class="overlay"></div>
+            <div className="content">
+                <div className="left">
+                    <span>{props.viewcount}k</span>
+                    <span>Viewed users</span>
+                </div>
+                <img src={add} alt="Add Icon" id="add-icon"/>
+            </div>
+        </div>
+    )
+}
