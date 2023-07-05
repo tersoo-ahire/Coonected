f8560fa1aee52dcbdf8616880f273e51e94d8902
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-05 23:37:13 +0100

    created lifestyle component for explore page

--- website/src/components/Lifestyle.jsx
+++ website/src/components/Lifestyle.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import LifestyleImage from "./LifestyleImage"
+import lifestyledata from "../data/lifestyledata"
 
 export default function Lifestyle(){
+    const lifestylelist = lifestyledata.map(item => (<LifestyleImage key={item.id} {...item} />))
+
     return (
-        <div>
-            Lifestyle
+        <div className="media-container">
+            {lifestylelist}
         </div>
     )
 }
---
+++ website/src/data/lifestyledata.js
@@ -0,0 +1,66 @@
+import img1 from "../assets/lifestyle/img1.png"
+import img2 from "../assets/lifestyle/img2.png"
+import img3 from "../assets/lifestyle/img3.png"
+import img4 from "../assets/lifestyle/img4.png"
+import img5 from "../assets/lifestyle/img5.png"
+import img6 from "../assets/lifestyle/img6.png"
+import img7 from "../assets/lifestyle/img7.png"
+import img8 from "../assets/lifestyle/img8.png"
+import img9 from "../assets/lifestyle/img9.png"
+
+export default [
+    {
+        id: 1,
+        username: "Image1",
+        image: img1,
+        viewcount: 150,
+    },
+    {
+        id: 2,
+        username: "Image2",
+        image: img2,
+        viewcount: 110,
+    },
+    {
+        id: 3,
+        username: "Image3",
+        image: img3,
+        viewcount: 100,
+    },
+    {
+        id: 4,
+        username: "Image4",
+        image: img4,
+        viewcount: 180,
+    },
+    {
+        id: 5,
+        username: "Image5",
+        image: img5,
+        viewcount: 200,
+    },
+    {
+        id: 6,
+        username: "Image6",
+        image: img6,
+        viewcount: 170,
+    },
+    {
+        id: 7,
+        username: "Image7",
+        image: img7,
+        viewcount: 250,
+    },
+    {
+        id: 8,
+        username: "Image8",
+        image: img8,
+        viewcount: 50,
+    },
+    {
+        id: 9,
+        username: "Image9",
+        image: img9,
+        viewcount: 75,
+    },
+]
