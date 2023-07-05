25bbf2660bc077ed832596ebbc1c69c209b730e1
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-05 23:46:12 +0100

    created events component to be rendered on explore page

--- website/src/components/EventsExplore.jsx
+++ website/src/components/EventsExplore.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import EventsImage from "./EventsImage"
+import eventsdata from "../data/eventsdata"
 
 export default function EventsExplore(){
+    const eventslist = eventsdata.map(item => (<EventsImage key={item.id} {...item} />))
+
     return (
-        <div>
-            Events
+        <div className="media-container">
+            {eventslist}
         </div>
     )
 }
