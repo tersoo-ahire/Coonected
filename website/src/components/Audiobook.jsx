bbd577fdfe6314c4631d77ca51176d8fce9b2f9b
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-07 10:06:57 +0100

    added logic to dynamically render appropriate rating image based on rating count in the data.

--- website/src/components/Audiobook.jsx
+++ website/src/components/Audiobook.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import stars from "../assets/books/stars.svg";
+import starImage from "../assets/books/star.svg";
 
 export default function Audiobook(props){
+
+    const ratings = () => {
+        const stars = [];
+        for (let i = 1; i <= props.rating; i++) {
+          stars.push(<img key={i} src={starImage} alt="Stars" />);
+        }
+        return stars;
+    };
+
     return (
         <div className="ebooks-sub">
             <div className="image-container">
                 <img src={props.book_image} alt={props.book_title} />
-                <div class="overlay"></div>
+                <div className="overlay"></div>
             </div>
             <h2>{props.book_title}</h2>
             <div className="details-container">
                 <div className="left">
-                    <img src={stars} alt="Stars" />
+                    <div className="ratings">
+                        {ratings()}
+                    </div>
                     <span>
                         {props.read_count} read this book
                     </span> 
